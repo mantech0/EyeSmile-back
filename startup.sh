c77218545a18965fc01977e645dfb5891f6cf462
mantech0 <man0519tech@gmail.com>
2025-03-10 00:34:11 +0900

    Fix: Update backend deployment configuration and startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd src
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --timeout 600 
