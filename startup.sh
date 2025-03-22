c41e9f49aa589a4b3231b620ae8867918f7cce17
mantech0 <man0519tech@gmail.com>
2025-03-22 21:45:27 +0900

    feat: カメラ機能の実装とアンケート機能の改善

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd src
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --timeout 600 
+cd /home/site/wwwroot
+export PYTHONPATH=/home/site/wwwroot
+gunicorn src.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --timeout 120 --access-logfile - --error-logfile - --log-level info 
