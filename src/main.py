5fb25770e9a02d4a05ee67020325e09e668e79e0
mantech0 <man0519tech@gmail.com>
2025-03-23 17:10:42 +0900

    feat: カメラ機能の実装とアンケート機能の改善

--- src/main.py
+++ src/main.py
@@ -38,7 +38,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://localhost:8080"
+        "http://localhost:8080",
+        "*"  # 開発中は一時的にすべてのオリジンを許可
     ],
     allow_credentials=True,
     allow_methods=["*"],
