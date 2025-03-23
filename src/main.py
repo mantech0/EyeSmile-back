a57f930710f209a3f6f3e170ea6c13ed99e168ed
mantech0 <man0519tech@gmail.com>
2025-03-23 18:03:38 +0900

    feat: カメラ機能の実装とアンケート機能の改善

--- src/main.py
+++ src/main.py
@@ -38,13 +38,13 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://localhost:8080",
-        "*"  # 開発中は一時的にすべてのオリジンを許可
+        "http://localhost:8080"
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
-    expose_headers=["*"]
+    expose_headers=["*"],
+    max_age=3600
 )
 
 @app.get("/api/v1/health")
